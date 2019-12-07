df40fced857a7265c6f7a8123de592f1e3c9b2c6
Heather <heegraphicgirl@gmail.com>
2019-12-06 19:40:10 -0700

    hamb menu needs a fix but GRRR

--- 10-Swag_Shop/js/scripts.js
+++ 10-Swag_Shop/js/scripts.js
@@ -1,3 +1,12 @@
-function toggleMenu() {
+/* function toggleMenu() {
   document.getElementById("smallNav").classList.toggle("hide");
 }
+ */
+//Create a button and give it an ID of "smallNav"
+//Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
+function toggleMenu() {
+  document.getElementById("smallNav").classList.toggle("open");
+  document.getElementById("primaryNav").classList.toggle("open");
+}
+var x = document.getElementById('smallNav');
+x.onclick = toggleMenu; 
